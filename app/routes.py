918abc079cec91c719138f648b5fda3bdc6108b3
Nicolas Signolet <nicolas@bcisoft.fr>
2022-04-08 15:30:27 +0200

    delete content after opening

--- app/routes.py
+++ app/routes.py
@@ -41,8 +41,6 @@ def home():
     return render_template("pages/home.html", ots_id=create_id())
 
 
-
-
 @app.route("/create-secret", methods=['GET', 'POST'])
 def create_secret():
     if request.method == 'POST':
@@ -51,10 +49,12 @@ def create_secret():
         lifetime = int(data['ots_lifetime'])
         print(datetime.utcnow())
         date_de_fin = datetime.utcnow() + timedelta(days=lifetime)
-        new_ots = OTS(ots_id=data['ots_id'], ots_content=data['ots_content'], ots_key2=data['ots_key2'], ots_date_fin=date_de_fin)
+        new_ots = OTS(ots_id=data['ots_id'], ots_content=data['ots_content'], ots_key2=data['ots_key2'],
+                      ots_date_fin=date_de_fin)
         db.session.add(new_ots)
         db.session.commit()
         return {"message": f"Your message is registrated!!"}
+
 
 @app.route("/purge", methods=['GET'])
 def purge():
@@ -63,27 +63,30 @@ def purge():
         db.session.commit()
         return {"message": f"Purging performed!"}
 
+
 @app.route('/get_ots/<ots_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def get_ots(ots_id):
     if request.method == 'GET':
         return display_ots(ots_id=ots_id)
 
-@app.route('/get_ots/display_ots', methods = ['GET', 'POST'])
+
+@app.route('/get_ots/display_ots', methods=['GET', 'POST'])
 def display_ots(ots_id):
     ots_exists = bool(OTS.query.filter_by(ots_id=ots_id).first())
     if ots_exists:
         ots_to_display = OTS.query.get(ots_id)
-        currrent_time = datetime.utcnow()
+        db.session.query(OTS).filter(OTS.ots_id == ots_id).delete()
+        db.session.commit()
+        current_time = datetime.utcnow()
         date_fin = ots_to_display.ots_date_fin
-        if date_fin >= currrent_time:
-
+        if date_fin >= current_time:
             return render_template("pages/display_ots.html", ots_id=str(ots_to_display.ots_id),
-                               ots_content=ots_to_display.ots_content, ots_key2=ots_to_display.ots_key2,
-                               ots_date_fin=ots_to_display.ots_date_fin)
+                                   ots_content=ots_to_display.ots_content, ots_key2=ots_to_display.ots_key2,
+                                   ots_date_fin=ots_to_display.ots_date_fin)
         else:
-           #==================================================#
+            # ==================================================#
             # return render_template ("pages/error.html")
-           # ==================================================#
+            # ==================================================#
             return "Il y a eu un problème lors de la lecture de votre message"
     else:
         # ==================================================#
